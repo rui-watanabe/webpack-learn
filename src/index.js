1d86e4de368368bd5fe1285eda10f82dd4e1989c
rui-watanabe <wataru610806@gmail.com>
2020-03-31 22:44:17 +0900

    implement url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash'
 import './style.css'
+import logo from './favicon.ico';
 
 function component() {
   const element = document.createElement("div");
   const array = ['hello', 'webpack', '!!'];
-  element.innerHTML = _.join(array, ' ')
+  element.innerHTML = _.join(array, ' ');
   return element;
 }
 
 document.body.appendChild(component());
-document.body.classList.add('haikei')
+document.body.classList.add('haikei');
+
+const image = new Image();
+image.src = logo;
+document.body.appendChild(image);
